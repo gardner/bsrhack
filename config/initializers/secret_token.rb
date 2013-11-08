1077f0f4defe3f849084b70f4d33f84b5338500e
gardner <gbickfor@adobe.com>
2013-11-07 20:39:25 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bsrhack::Application.config.secret_key_base = '655a6050fe71b48df937bb359c123f0abe2c59dcbb542889c618d5f72a464aed3a60582959a5c011d16b8c5e118588979c9a9f0aedeab3d9622c5dcbec8830f0'
